fb95ccbb6949399afa9a9083c3f6477d6f63d5a2
Bruno Mariani <bruno0907@gmail.com>
2021-01-21 23:01:01 -0300

    Removal of dist folder from deploy

--- dist/services/AdminAuthService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AdminAuthService {
-    constructor() {
-        this.execute = async () => { };
-    }
-}
-exports.default = new AdminAuthService;
